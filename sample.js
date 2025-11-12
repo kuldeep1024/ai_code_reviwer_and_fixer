24032a4a93e007e9f37b68b158defc1beb3ba68d
kuldeep1024 <kunwar.kuldeep@gmail.com>
2025-11-12 15:53:44 +0530

    added loop

--- sample.js
+++ sample.js
@@ -32,6 +32,4 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // --- Simple Loop Example ---
-for (let i = 1; i <= 5; i++) {
-  console.log(`Counting: ${i}`);
-}
+for (let i=1;i<=5;i++){ console.log("Counting:",i) }
